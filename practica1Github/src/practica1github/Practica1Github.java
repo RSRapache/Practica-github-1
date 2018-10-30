01d793e27dc16cd815850feec1b9c7f23e43a57f
admin <admin@Win7_1TB>
2018-10-30 09:29:47 +0100

    Inicial

---
+++ practica1Github/src/practica1github/Practica1Github.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1github;
+import java.util.*;
+/**
+ *
+ * @author admin
+ */
+public class Practica1Github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc=new Scanner(System.in);
+        
+        String cadena ,caracter;
+        int cont=0;
+        
+        System.out.println("Escriba la cadena");
+        cadena=sc.nextLine();
+        
+        do{
+        
+            System.out.println("Escriba letra");
+            caracter=sc.nextLine();
+            
+            caracter=caracter.substring(0, 1);
+            
+            
+        }while(!Character.isLetter(caracter.charAt(0)));
+        
+        for(int i=0;i<cadena.length();i++){
+            
+            if(caracter.equalsIgnoreCase(cadena.substring(i, i+1)))
+                cont++;
+        }
+        
+        System.out.println(cont+" veces que sale la letra "+caracter+" en la cadena "+cadena);
+    }
+    
+}
